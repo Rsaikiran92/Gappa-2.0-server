6374096ea402473d25831a6695236d4d8385fe77
TestingGappa <140157699+TestingGappa@users.noreply.github.com>
2023-07-25 14:36:38 +0530

    Update user.model.js

--- Models/user.model.js
+++ Models/user.model.js
@@ -4,6 +4,11 @@ const templateSchema = new mongoose.Schema({
   content: { type: String },
 });
 
+const answerSchema = new mongoose.Schema({
+  question: [],
+  answer: [],
+});
+
 const groupSchema = new mongoose.Schema({
   groupId: { type: String, required: true },
   groupName: { type: String, required: true },
@@ -13,12 +18,25 @@ const groupSchema = new mongoose.Schema({
   template: [templateSchema],
 });
 
+const communitySchema = new mongoose.Schema({
+  groupName: { type: String, required: true },
+  description: { type: String },
+  banner: { type: String },
+  displayProfile: { type: String },
+  groupRules: { type: String },
+  paid: { type: Boolean, required: true },
+  groupLink: { type: String, required: true },
+  questionSet: [],
+  answerSet: [answerSchema],
+});
+
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   whatsappNumber: { type: Number, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   group: [groupSchema],
+  community: [communitySchema],
 });
 
 const userModel = mongoose.model("user", userSchema);
