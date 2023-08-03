85ea3223aef3edb9bd0a0a8df2413e21f4a27ba9
R Sai kiran <103918668+Rsaikiran92@users.noreply.github.com>
2023-08-03 14:25:32 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -132,7 +132,7 @@ userRouter.get("/:userId/groups/:groupId", async (req, res) => {
   }
 
   // Find the group with the specified groupId from the user's group array
-  const group = user.group.find((g) => g._id.toString() === groupId);
+  const group = user.group.find((g) => g.groupId.toString() === groupId);
 
   if (!group) {
     return res.status(404).json({ error: "Group not found" });
