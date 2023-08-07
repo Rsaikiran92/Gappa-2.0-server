5c18d72eff4fc60937dbdd3b80351acc058b63fc
R Sai kiran <103918668+Rsaikiran92@users.noreply.github.com>
2023-08-07 11:24:08 +0530

    Update db.js

--- Configs/db.js
+++ Configs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect("mongodb+srv://gappa:NJCI8wzsNuWNjGpY@cluster0.oqj5yw1.mongodb.net/BM_App?retryWrites=true&w=majority");
+const connection = mongoose.connect("mongodb+srv://gappa:NJCI8wzsNuWNjGpY@cluster0.oqj5yw1.mongodb.net/gappa?retryWrites=true&w=majority");
 
 module.exports = { connection };
